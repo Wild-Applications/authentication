c5e786f725502bb83b37126690fa8bac683c4b23
mewild <michael@wildapplications.com>
2017-09-27 15:40:17 +0100

    Latest Updates

--- src/helpers/authentication.helper.js
+++ src/helpers/authentication.helper.js
@@ -5,7 +5,8 @@
 var mysql      = require('mysql');
 var pool = mysql.createPool({
   connectionLimit   :  10,
-  host              : 'hashdb.c5mqjhqvtirx.us-west-2.rds.amazonaws.com',
+  //host              : 'hashdb.c5mqjhqvtirx.us-west-2.rds.amazonaws.com',
+  host              : 'hash.c5mqjhqvtirx.us-west-2.rds.amazonaws.com',
   port              : '3306',
   user              : 'wildappsadmin',
   password          : '180770150270',
@@ -23,20 +24,16 @@ authenticator.authenticate = function(call, callback){
     //fetch hash
     pool.getConnection(function(err, connection) {
       if (err) {
-        console.log("Problem connecting to the database");
-        callback({message:'0001 - Failed to connect to the database'},null);
+        return callback({message:JSON.stringify({code:'0001', message:'Failed to connect to database'})}, null);
       }else{
-        console.log("connected to the database");
         var query = "SELECT hash FROM hashes WHERE _id = '" + call.request._id + "'";
         connection.query(query, function(error, results){
           connection.release();
           if(err){
-            console.log("Error running query for hash");
-            callback({message:'0002 - Failed to run query against the database'},null);
+            return callback({message:JSON.stringify({code:'0002', message:'Failed to run query against the database'})}, null);
           }else{
             if(typeof results != 'undefined'){
               if(results.length != 0){
-                console.log("got hash");
                 var body = {};
                 body.password = call.request.password;
                 body.hash = results[0].hash;
@@ -47,20 +44,18 @@ authenticator.authenticate = function(call, callback){
                 var encryptionClient = new encryptionDescriptor.EncryptionService('service.encryption:1295', grpc.credentials.createInsecure());
                 encryptionClient.checkPassword(body,function(err, response){
                   if(err){
-                    console.log("problem checking the password");
                     callback(err,null);
                   }else{
-                    console.log("password was checked");
                     console.log(response.match);
                     callback(null,{authenticated:response.match});
                   }
                 });
               }else{
                 //no results
-                callback({message:'0012 - No user exists with that id'},null);
+                return callback({message:JSON.stringify({code:'0012', message:'Username and password did not match'})}, null);
               }
             }else{
-              callback({message:'0012 - No user exists with that id'},null);
+              return callback({message:JSON.stringify({code:'0012', message:'Username and password did not match'})}, null);
             }
           }
         });
@@ -74,13 +69,11 @@ authenticator.store = function(call,callback){
   if(call.request._id && call.request.password){
     pool.getConnection(function(err, connection){
       if (err) {
-        callback({message:'0001 - Failed to connect to the database'},null);
-        return;
+        return callback({message:JSON.stringify({code:'0001', message:'Failed to connect to the database'})}, null);
       }else{
         connection.beginTransaction(function(err){
           if(err){
-            callback({message:'0001 - Failed to connect to the database'}, null);
-            return;
+            return callback({message:JSON.stringify({code:'0001', message:'Failed to connect to the database'})}, null);
           }
           //hash the password
           var grpc = require("grpc");
@@ -95,12 +88,17 @@ authenticator.store = function(call,callback){
               connection.query(query, function(err, results){
                 if(err){
                   connection.rollback(function(){
-                    callback({message:"testing"},null);
+                    return callback(err, null);
                     return;
                   });
                 }else{
-                  //password stored successfully
-                  callback(null, {stored:true});
+                  connection.commit(function(err){
+                    if(err){
+                      callback(err, null);
+                    }else{
+                      callback(null, {stored: true});
+                    }
+                  })
                 }
               });
             }
@@ -110,8 +108,7 @@ authenticator.store = function(call,callback){
       }
     });
   }else{
-    callback({message:"0007 - Not all parameters were supplied"},null);
-    return;
+    return callback({message:JSON.stringify({code:'0007', message:'Not all parameters were supplied'})}, null);
   }
 }
 
